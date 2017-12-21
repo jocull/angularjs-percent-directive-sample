27bca3465670b7ff83c549a1c727ae6e1e2456a6
James O'Cull <jocull@delmarsd.com>
2017-12-21 00:10:29 -0500

    Directive functionality and tests

--- app/app.module.js
+++ app/app.module.js
@@ -3,14 +3,35 @@
   angular
     .module('percentApp', [])
     .controller('PercentController', function PercentController($scope) {
-      $scope.percent = 0.12345; // default
+      if (typeof $scope.percent === 'undefined') {
+        $scope.percent = null; // default
+      }
     })
-    .directive('percent', function percentDirective() {
+    .directive('percent', function percentDirective($filter) {
+      function link(scope, element, attrs) {
+        var numberFilter = $filter('number');
+
+        function update(value) {
+          var n = parseFloat(value);
+          if (!isNaN(n)
+            && n >= 0
+            && n <= 1) {
+
+            element.text(numberFilter(n * 100) + '%');
+            scope.valid = true;
+          } else {
+            element.text('Invalid input!');
+            scope.valid = false;
+          }
+        }
+
+        scope.$watch(attrs.value, function (newValue) {
+          update(newValue);
+        });
+      }
+
       return {
-        template: '{{model * 100 | number}}%',
-        scope: {
-          model: '=value',
-        },
+        link,
       };
     });
 
--- app/app.module.spec.js
+++ app/app.module.spec.js
@@ -2,16 +2,46 @@ describe('PercentController', function () {
 
   beforeEach(module('percentApp'));
 
-  it ('test running should be all cool', function() {
-    expect(true).toBe(true);
-  });
+  it('should create an empty percent controller', inject(function ($controller, $rootScope) {
+    var childScope = $rootScope.$new();
+    var ctrl = $controller('PercentController', { $scope: childScope });
 
-  it('should create an empty percent controller', inject(function ($controller) {
-    var scope = {};
-    var ctrl = $controller('PercentController', { $scope: scope });
-
-    expect(scope.percent).toBeGreaterThanOrEqual(0);
-    expect(scope.percent).toBeLessThanOrEqual(1);
+    expect(childScope.percent).toBeNull();
   }));
+
+});
+
+describe('PercentDirective', function () {
+
+  beforeEach(module('percentApp'));
+
+  beforeEach(inject(function (_$compile_, _$rootScope_) {
+    // The injector unwraps the underscores (_) from around
+    // the parameter names when matching.
+    $compile = _$compile_;
+    $scope = _$rootScope_.$new();
+  }));
+
+  function testElement(input, output) {
+    var childScope = $scope.$new();
+    childScope.percent = input;
+
+    var element = $compile('<percent value="percent">')(childScope);
+    childScope.$digest();
+
+    return element.text();
+  }
+
+  it('replaces element with content', function () {
+    expect(testElement(0)).toBe('0%');
+    expect(testElement(0.12345)).toBe('12.345%');
+    expect(testElement(0.5)).toBe('50%');
+    expect(testElement(1)).toBe('100%');
+
+    expect(testElement(-1)).not.toBe('-100%');
+    expect(testElement(2)).not.toBe('200%');
+
+    expect(testElement('not a number')).not.toBe('');
+  });
 
 });
